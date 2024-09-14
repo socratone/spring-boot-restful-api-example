fe55b90bb817a397f7af04908ac3af9ff70f3caf
Kiwon Kim <gim2origin@gmail.com>
2024-09-14 22:08:36 +0900

    feat: init

---
+++ src/main/java/com/socratone/spring_boot_restful_api/SpringBootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.socratone.spring_boot_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socratone/spring_boot_restful_api/SpringBootRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socratone.spring_boot_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
